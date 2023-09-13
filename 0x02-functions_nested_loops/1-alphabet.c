c1278ba77a9e0572578636129375e44e7eb3db69
safaakan <safaaka939@gmail.com>
2023-09-12 17:10:45 -0700

    tasks1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+  * print_alphabet - Make the alphabet
+  *
+  * Return: void
+  */
+void print_alphabet(void)
+{
+char ch = 'a';
+while (ch <= 'z')
+{
+_putchar(ch);
+ch++;
+}
+_putchar('\n');
+
+return (0);
+}
