5b7ceb4bca23fb6267d637e500e62f46d9627a16
safaakan <safaaka939@gmail.com>
2023-10-02 18:34:10 -0700

    tasks0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Prints the number of arguments passed into main
+ * @argc: Number of command line arguments
+ * @argv: Array name
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+printf("%s\n", argv[argc * 0]);
+return (0);
+}
