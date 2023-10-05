774652ed32fc43c840067fb6e94cd023067bdc05
safaakan <safaaka939@gmail.com>
2023-10-05 14:55:39 -0700

    tasks6

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -10,92 +10,35 @@
 
 char **strtow(char *str)
 {
-char **split;
-int i, j = 0, temp = 0, size = 0, words = num_words(str);
+	char **d;
+	int i;
+	int j = 0;
+	int con = 0;
 
-if (words == 0)
-return (NULL);
-split = (char **) malloc(sizeof(char *) * (words + 1));
-if (split != NULL)
-{
-for (i = 0; i <= len(str) && words; i++)
-{
-if ((str[i] != ' ') && (str[i] != '\0'))
-size++;
-else if (((str[i] == ' ') || (str[i] == '\0')) && i && (str[i - 1] != ' '))
-{
-split[j] = (char *) malloc(sizeof(char) * size + 1);
-if (split[j] != NULL)
-{
-while (temp < size)
-{
-split[j][temp] = str[(i - size) +temp];
-temp++;
-}
-split[j][temp] = '\0';
-size = temp = 0;
-j++;
-}
-else
-{
-while (j-- >= 0)
-free(split[j]);
-free(split);
-return (NULL);
-}
-}
-}
-split[words] = NULL;
-return (split);
-}
-else
-return (NULL);
-}
+	if (str == NULL)
+		return (NULL);
 
-/**
- * num_words - counts the number of words in str
- *@str: string to be used
- *
- *Return: number of words
- */
-int num_words(char *str)
-{
-int i = 0, words = 0;
+	for (i = 0 ; str[i] != '\0' ; i++)
+	{
+		if (str[i] != 32)
+			con++;
+	}
 
-while (i <= len(str))
-{
-if ((str[i] != ' ') && (str[i] != '\0'))
-{
-i++;
-}
-else if (((str[i] == ' ') || (str[i] == '\0')) && i && (str[i - 1] != ' '))
-{
-words += 1;
-i++;
-}
-else
-{
-i++;
-}
-}
-return (words);
-}
+	d = malloc(sizeof(char) * con);
 
-/**
- * len - returns length of str
- *@str: string to be counted
- *
- * Return: length of the string
- */
+	if (d == NULL)
+		return (NULL);
 
-int len(char *str)
-{
-int len = 0;
-
-if (str != NULL)
-{
-while (str[len])
-len++;
-}
-return (len);
+	for (i = 0 ; str[i] != '\0' ; i++)
+	{
+		if (str[i] != 32)
+		{
+			*d[j] = str[i];
+			j++;
+		}
+		else
+		{
+		}
+	}
+	return (d);
 }
