219b63ce0627cb8e6f4342d38d9ca88f5181dc43
safaakan <safaaka939@gmail.com>
2023-10-04 18:17:05 -0700

    tasks1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _strdup - create an array of chars
+ * @str: char
+ *
+ * Return: char
+ */
+char *_strdup(char *str)
+{
+int length, i;
+	char *arr;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+
+	length = 0;
+	while (*(str + length) != '\0')
+	{
+		length++;
+	}
+
+	arr = malloc(length * sizeof(char) + 1);
+	if (arr == NULL)
+		return (NULL);
+
+	for (i = 0; i < length; i++)
+	{
+		arr[i] = str[i];
+	}
