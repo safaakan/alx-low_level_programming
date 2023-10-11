9827b8bbdf92f54d073a481522716e835368d9f9
safaakan <safaaka939@gmail.com>
2023-10-10 17:02:49 -0700

    tasks2

--- 0x0D-preprocessor/02-main.c
+++ 0x0D-preprocessor/02-main.c
@@ -1,10 +1,13 @@
-#include<stdio.h>
-#define filename "2-main.c"
-
+#include <stdio.h>
+#define __FILE_NAME__ "2-main.c"
+/**
+ * main - prints the name of the file it was compiled from
+ *
+ * Return: Everything worked ok
+ */
 
 int main(void)
 {
-printf("%s\n", filename);
-
-return(0);
+	printf("%s\n", __FILE_NAME__);
+	return (0);
 }
--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,10 +1,13 @@
-#include<stdio.h>
-#define filename "2-main.c"
+#include <stdio.h>
 
+/**
+ * main - prints the name of the file it was compiled from
+ *
+ * Return: Everything worked ok
+ */
 
 int main(void)
 {
-printf("%s\n", filename);
-
-return(0);
+	printf("%s\n", __BASE_FILE__);
+	return (0);
 }
