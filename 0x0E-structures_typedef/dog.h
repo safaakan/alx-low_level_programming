1f002d5e5459ae5ba1ca6ce74b702e9bde3f20f3
safaakan <safaaka939@gmail.com>
2023-10-10 17:43:52 -0700

    tasks3

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -13,9 +13,13 @@ typedef struct dog
 	char *name;
 	float age;
 	char *owner;
-}
+}dog_t;
 
+/**
+ * dog_t - Typedef for dog structure
+ */
 typedef struct dog dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 
 void print_dog(struct dog *d);
