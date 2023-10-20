1bac1e09c1e73df9b18907dfdd2ff23d6946182a
safaakan <safaaka939@gmail.com>
2023-10-20 15:15:41 -0700

    tasks1

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,8 +1,8 @@
 #ifndef LISTS_H
-#define LISTS__H
-#define safe_free(p) _free((void **) &(p))
-#include <stdio.h>
+#define LISTS_H
+
 #include <stdlib.h>
+#include <stdio.h>
 #include <string.h>
 
 /**
@@ -15,18 +15,14 @@
  */
 typedef struct list_s
 {
-    char *str;
-    unsigned int len;
-    struct list_s *next;
+char *str;
+unsigned int len;
+struct list_s *next;
 } list_t;
 
-int _putchar(char c) ;
-int _strlen(char *s);
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
-void _free(void **ptr);
-
 #endif
