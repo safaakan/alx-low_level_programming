42add003493522c5bd8d959cd7d853618995e11c
safaakan <safaaka939@gmail.com>
2023-10-24 14:35:47 -0700

    tasks0

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -6,19 +6,18 @@
 #include <string.h>
 
 /**
- * struct list_s - singly linked list
- * @str: string - (malloc'ed string)
- * @len: length of the string
+ * struct listint_s - singly linked list
+ * @n: integer
  * @next: points to the next node
  *
  * Description: singly linked list node structure
+ * 
  */
-typedef struct list_s
+typedef struct listint_s
 {
-char *str;
-unsigned int len;
-struct list_s *next;
-} list_t;
+    int n;
+    struct listint_s *next;
+} listint_t;
 
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
