dfc373a3163acd9b079f6b386c35e1d1540082be
safaakan <safaaka939@gmail.com>
2023-10-24 15:17:57 -0700

    tasks0

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -9,14 +9,14 @@
  */
 size_t print_listint(const listint_t *h)
 {
-	size_t nodes = 0;
+	size_t i = 0;
 
 	while (h)
 	{
-		nodes++;
 		printf("%d\n", h->n);
 		h = h->next;
+		i++;
 	}
 
-	return (nodes);
+	return (i);
 }
